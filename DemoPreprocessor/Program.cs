e4604210e2b0d70022c2c4ca30e4597eb7c3337e
Davide Albieni <dalbieni@geiweb.it>
2016-01-30 18:01:40 +0100

    Primo commit

---
+++ DemoPreprocessor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoPreprocessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(PreprocessorClass.SayHello());
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+
+        }
+    }
+}
